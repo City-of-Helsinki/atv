d50fb1153917cca6bdfdee55a582ec001842a487
Juha Louhiranta <juha.louhiranta@anders.fi>
2021-06-15 11:57:46 +0300

    Add API key generation for a service
    API key functionality is accomplished by using
    the Django REST Framework API Key lirary.
    
    Refs ATV-2

--- services/models.py
+++ services/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from rest_framework_api_key.models import AbstractAPIKey
 
 
 class TimestampedModel(models.Model):
@@ -18,5 +19,17 @@ class Service(TimestampedModel):
         verbose_name = _("service")
         verbose_name_plural = _("services")
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
+
+
+class ServiceAPIKey(AbstractAPIKey):
+    service = models.ForeignKey(
+        Service,
+        on_delete=models.CASCADE,
+        related_name="api_keys",
+    )
+
+    class Meta(AbstractAPIKey.Meta):
+        verbose_name = _("service API key")
+        verbose_name_plural = _("service API keys")
---
+++ services/tests/factories.py
@@ -0,0 +1,29 @@
+import factory
+
+from services.models import Service, ServiceAPIKey
+
+
+class ServiceFactory(factory.django.DjangoModelFactory):
+    name = factory.Sequence(lambda n: "service %d" % n)
+
+    @factory.lazy_attribute
+    def description(self):
+        return f"{self.name} description"
+
+    class Meta:
+        model = Service
+
+
+class ServiceAPIKeyFactory(factory.django.DjangoModelFactory):
+    service = factory.SubFactory(ServiceFactory)
+    name = factory.Sequence(lambda n: "api key %d" % n)
+
+    class Meta:
+        model = ServiceAPIKey
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        """Use correct manager method for APIKey."""
+        manager = cls._get_manager(model_class)
+        obj, key = manager.create_key(*args, **kwargs)
+        return obj
