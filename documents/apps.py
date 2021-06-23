3989d601e68934fb359e9ba590ef64ffe07b1fb0
Juha Louhiranta <juha.louhiranta@anders.fi>
2021-06-23 11:50:06 +0300

    Move TimestampedModel to utils module
    Refs ATV-1

---
+++ documents/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class DocumentsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "documents"
+    verbose_name = _("documents")
---
+++ utils/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(verbose_name=_("created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name=_("updated at"), auto_now=True)
+
+    class Meta:
+        abstract = True
