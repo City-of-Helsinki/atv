83fd53a28e55698fa40c20ead971eb0bab1f8534
Alejandro Ramirez <armzprz@gmail.com>
2021-08-30 11:09:28 +0300

    Add permission checking for API

--- documents/api.py
+++ documents/api.py
@@ -3,13 +3,14 @@ from django.db import transaction
 from rest_framework import status
 from rest_framework.exceptions import NotAuthenticated
 from rest_framework.parsers import MultiPartParser
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_extensions.mixins import NestedViewSetMixin
 
+from atv.decorators import service_api_key_required, staff_required
+from services.enums import ServicePermissions
 from services.models import Service, ServiceAPIKey
-from services.permissions import HasServiceAPIKey
 
 from .models import Attachment, Document
 from .serializers import (
@@ -20,18 +21,95 @@ from .serializers import (
 
 
 class AttachmentViewSet(ModelViewSet, NestedViewSetMixin):
-    queryset = Attachment.objects.all()
     permission_classes = [IsAdminUser]
     serializer_class = AttachmentSerializer
 
+    def get_queryset(self):
+        """
+        Only allow for the user to see Attachments that belong to their Documents
+        or their Service.
+
+        If the user is:
+            - superuser, they can see everything
+            - staff, they can see only Attachments for Documents of their Service
+            - authenticated, they can see only their own Attachments
+            - anonymous (no valid authentication), they can't see anything
+        """
+        user = self.request.user
+
+        # If the user is a superuser, return the whole set
+        if user.is_superuser:
+            return Attachment.objects.all()
+
+        # If the user is anonymous, don't return anything,
+        # even if they're associated to a Service.
+        if user.is_anonymous:
+            return Attachment.objects.none()
+
+        filters = {}
+
+        # Filter the Documents only for the user's Service
+        if service := self.request.service:
+            filters["document__service"] = service
+
+        # If the user doesn't have permissions to view that Service,
+        # only show the Documents that belong to them
+        if not user.has_perm(
+            ServicePermissions.VIEW_ATTACHMENTS.value, filters["service"]
+        ):
+            filters = {"document__user_id": user.id}
+
+        return Attachment.objects.filter(**filters)
+
 
 class DocumentViewSet(ModelViewSet):
-    queryset = Document.objects.all()
-    permission_classes = [HasServiceAPIKey | IsAdminUser]
     parser_classes = [MultiPartParser]
     serializer_class = DocumentSerializer
+    # Permission checking is done by the decorators on a method basis
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        """
+        Only allow for the user to see Documents that belong to them or their Service.
+
+        If the user is:
+            - superuser, they can see everything
+            - staff, they can see only Documents for their Service
+            - authenticated, they can see only their own Documents
+            - anonymous (no valid authentication), they can't see anything
+        """
+        user = self.request.user
+
+        # If the user is a superuser, return the whole set
+        if user.is_superuser:
+            return Document.objects.all()
+
+        # If the user is anonymous, don't return anything,
+        # even if they're associated to a Service.
+        if user.is_anonymous:
+            return Document.objects.none()
+
+        filters = {}
+
+        # Filter the Documents only for the user's Service
+        if service := self.request.service:
+            filters["service"] = service
+
+        # If the user doesn't have permissions to view that Service,
+        # only show the Documents that belong to them
+        if not user.has_perm(
+            ServicePermissions.VIEW_DOCUMENTS.value, filters["service"]
+        ):
+            filters = {"user_id": user.id}
+
+        return Document.objects.filter(**filters)
+
+    @staff_required(required_permission=ServicePermissions.VIEW_DOCUMENTS)
+    def list(self, request, *args, **kwargs):
+        return super(DocumentViewSet, self).list(request, *args, **kwargs)
 
     @transaction.atomic()
+    @service_api_key_required()
     def create(self, request, *args, **kwargs):
         try:
             key = request.META.get(settings.API_KEY_CUSTOM_HEADER)
