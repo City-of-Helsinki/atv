7daf3482ac5ea514f512870359e747dc6d6093ab
Alejandro Ramirez <armzprz@gmail.com>
2021-09-13 11:36:47 +0300

    Tweaks for Django Admin

--- users/models.py
+++ users/models.py
@@ -3,3 +3,6 @@ from helusers.models import AbstractUser
 
 class User(AbstractUser):
     audit_log_id_field = "uuid"
+
+    def __str__(self):
+        return f"{self.id} ({self.email or self.username or '-'})"
