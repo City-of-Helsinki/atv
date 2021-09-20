3f5686facca2499214a226f215174f103cb9b129
Alejandro Ramirez <armzprz@gmail.com>
2021-09-20 14:42:00 +0300

    Add tests for retrieving document permissions

---
+++ documents/tests/snapshots/snap_test_api_retrieve_document.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# snapshottest: v1 - https://goo.gl/zC4yUc
+from __future__ import unicode_literals
+
+from snapshottest import Snapshot
+
+snapshots = Snapshot()
+
+snapshots["test_list_document_service_user 1"] = {
+    "attachments": [],
+    "business_id": "",
+    "content": "",
+    "created_at": "2020-06-01T03:00:00+03:00",
+    "draft": False,
+    "id": "485af718-d9d1-46b9-ad7b-33ea054126e3",
+    "locked_after": None,
+    "metadata": {},
+    "status": "",
+    "tos_function_id": "81eee139736f4e52b046a1b27211c202",
+    "tos_record_id": "0f499febb6414e1dafc93febca5ef312",
+    "transaction_id": "bd3fd958-cfeb-4ab1-bea4-5c058e8fee5c",
+    "type": "",
+    "updated_at": "2020-06-01T03:00:00+03:00",
+}
+
+snapshots["test_list_document_superuser 1"] = {
+    "attachments": [],
+    "business_id": "",
+    "content": "",
+    "created_at": "2020-06-01T03:00:00+03:00",
+    "draft": False,
+    "id": "485af718-d9d1-46b9-ad7b-33ea054126e3",
+    "locked_after": None,
+    "metadata": {},
+    "status": "",
+    "tos_function_id": "81eee139736f4e52b046a1b27211c202",
+    "tos_record_id": "0f499febb6414e1dafc93febca5ef312",
+    "transaction_id": "bd3fd958-cfeb-4ab1-bea4-5c058e8fee5c",
+    "type": "",
+    "updated_at": "2020-06-01T03:00:00+03:00",
+}
+
+snapshots["test_retrieve_document_owner 1"] = {
+    "attachments": [],
+    "business_id": "",
+    "content": "",
+    "created_at": "2020-06-01T03:00:00+03:00",
+    "draft": False,
+    "id": "485af718-d9d1-46b9-ad7b-33ea054126e3",
+    "locked_after": None,
+    "metadata": {},
+    "status": "",
+    "tos_function_id": "81eee139736f4e52b046a1b27211c202",
+    "tos_record_id": "0f499febb6414e1dafc93febca5ef312",
+    "transaction_id": "bd3fd958-cfeb-4ab1-bea4-5c058e8fee5c",
+    "type": "",
+    "updated_at": "2020-06-01T03:00:00+03:00",
+}
