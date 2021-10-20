f890cb95d79fd212369571d7f86d1577754da87e
Juha Louhiranta <juha.louhiranta@anders.fi>
2021-10-21 00:31:19 +0300

    Make service permissions more fine-grained
    Manage permission is divided into add, change and delete
    permissions. Naming of the permissions is similar to
    Django's default persmissions.
    
    Refs ATV-87

--- services/enums.py
+++ services/enums.py
@@ -2,7 +2,11 @@ from django.db.models import TextChoices
 
 
 class ServicePermissions(TextChoices):
-    MANAGE_DOCUMENTS = "can_manage_documents", "Can manage documents"
+    ADD_DOCUMENTS = "can_add_documents", "Can add documents"
+    CHANGE_DOCUMENTS = "can_change_documents", "Can change documents"
+    DELETE_DOCUMENTS = "can_delete_documents", "Can delete documents"
     VIEW_DOCUMENTS = "can_view_documents", "Can view documents"
-    MANAGE_ATTACHMENTS = "can_manage_attachments", "Can manage attachments"
+    ADD_ATTACHMENTS = "can_add_attachments", "Can add attachments"
+    CHANGE_ATTACHMENTS = "can_change_attachments", "Can change attachments"
+    DELETE_ATTACHMENTS = "can_delete_attachments", "Can delete attachments"
     VIEW_ATTACHMENTS = "can_view_attachments", "Can view attachments"
