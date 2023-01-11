1f4b7ec5e1eaff4a2e7886ab5280f8cdb5499a20
Aki-Matti Antikainen <98747661+AntiAki-M@users.noreply.github.com>
2023-01-11 14:35:17 +0200

    ATV-142 Prevent user's names and emails from being saved to ATV user table
    All personal details regarding users should be inside encrypted content field of document

--- users/models.py
+++ users/models.py
@@ -6,3 +6,8 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.id} ({self.email or self.username or '-'})"
+
+    def clean(self):
+        super().clean()
+        # Prevent personal details from being saved to ATV user model, ATV doesn't need to know anything else than uuid
+        self.first_name = self.last_name = self.email = ""
