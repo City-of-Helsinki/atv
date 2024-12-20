fcc08d97d242fcaf013e6e3af67f6258ccad3169
Juha Louhiranta <juha.louhiranta@anders.com>
2024-12-20 09:51:16 +0200

    chore: fix issues reported by shellcheck

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Wait for the database
-if [ -z "$SKIP_DATABASE_CHECK" -o "$SKIP_DATABASE_CHECK" = "0" ]; then
+if [ -z "$SKIP_DATABASE_CHECK" ] || [ "$SKIP_DATABASE_CHECK" = "0" ]; then
     wait-for-it.sh "${DATABASE_HOST}:${DATABASE_PORT-5432}" --timeout=30
 fi
 
@@ -17,14 +17,14 @@ fi
 # Create admin user. Generate password if there isn't one in the environment variables
 if [[ "$CREATE_ADMIN_USER" = "1" ]]; then
     if [[ "$ADMIN_USER_PASSWORD" ]]; then
-      ./manage.py add_admin_user -u admin -p $ADMIN_USER_PASSWORD -e admin@hel.ninja
+      ./manage.py add_admin_user -u admin -p "$ADMIN_USER_PASSWORD" -e admin@hel.ninja
     else
       ./manage.py add_admin_user -u admin -e admin@hel.ninja
     fi
 fi
 
 # Start server
-if [[ ! -z "$@" ]]; then
+if [[ -n "$*" ]]; then
     "$@"
 elif [[ "$DEV_SERVER" = "1" ]]; then
     python -Wd ./manage.py runserver 0.0.0.0:8000
