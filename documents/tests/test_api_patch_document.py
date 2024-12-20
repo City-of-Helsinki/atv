2d0ad78630216807933b2bec082561d6f2f2e22e
Juha Louhiranta <juha.louhiranta@anders.com>
2024-12-20 16:06:55 +0200

    test: add missing test for updating Document.status_timestamp
    Refs: ATV-205

--- documents/tests/test_api_patch_document.py
+++ documents/tests/test_api_patch_document.py
@@ -6,6 +6,7 @@ import pytest
 from dateutil.relativedelta import relativedelta
 from dateutil.utils import today
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.utils import timezone
 from freezegun import freeze_time
 from guardian.shortcuts import assign_perm
 from rest_framework import status
@@ -684,3 +685,25 @@ def test_patch_status_update_in_multiple_formats(service_api_client, format):
     assert response.status_code == status.HTTP_200_OK
     document.refresh_from_db()
     assert document.status == "changed status"
+
+
+@freeze_time("2021-06-30T12:00:00")
+def test_patch_status_timestamp_is_updated(service_api_client):
+    """When updating the status of a document, the status_timestamp should also get
+    updated automatically.
+    """
+    document = DocumentFactory(
+        service=service_api_client.service,
+        status="testing",
+    )
+
+    with freeze_time("2024-12-20T12:00:00"):
+        dt = timezone.now()
+        response = service_api_client.patch(
+            reverse("documents-detail", args=[document.id]),
+            {"status": "changed status"},
+        )
+
+    assert response.status_code == status.HTTP_200_OK
+    document.refresh_from_db()
+    assert document.status_timestamp == dt
